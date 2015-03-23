181f921e30cb4b514744e7e98efcd97f874d6ebf
Alexander Skorulis <skorulis@gmail.com>
2015-03-23 19:22:08 +1100

    Initial Commit

---
+++ double-tap-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  double-tap-test
+//
+//  Created by Alexander Skorulis on 23/03/2015.
+//  Copyright (c) 2015 com.skorulis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ double-tap-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  double-tap-test
+//
+//  Created by Alexander Skorulis on 23/03/2015.
+//  Copyright (c) 2015 com.skorulis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
